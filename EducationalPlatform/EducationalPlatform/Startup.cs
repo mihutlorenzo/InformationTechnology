76ccb9dd3464a3f2e83cc9e2dd3cee0497509dab
maleacristina <maleacristinaecaterina@gmail.com>
2018-11-25 18:36:58 +0200

    Add project

---
+++ EducationalPlatform/EducationalPlatform/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EducationalPlatform.Startup))]
+namespace EducationalPlatform
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
