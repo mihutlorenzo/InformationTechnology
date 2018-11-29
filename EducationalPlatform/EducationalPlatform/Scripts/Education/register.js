f1ed02aee30562050642120eee976321b756ae57
maleacristina <maleacristinaecaterina@gmail.com>
2018-11-29 12:24:52 +0200

    Change values at dropdown list

---
+++ EducationalPlatform/EducationalPlatform/Scripts/Education/register.js
@@ -0,0 +1,10 @@
+﻿$("#RegisterViewModel_UserTypeId").change(function () {
+    var selectedValue = $('#RegisterViewModel_UserTypeId :selected').text();
+    var teacherDIV = $("#teacher");
+    if (selectedValue == "Teacher") {
+        teacherDIV.show();
+    } else {
+        teacherDIV.hide();
+    }
+
+});
