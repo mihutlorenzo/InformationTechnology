13da830d903c27ef7c5f64627393acce00d5f92e
Luca Ticusi <ticusi.luca@gmail.com>
2018-12-02 11:01:55 +0200

    Added student profile

--- EducationalPlatform/EducationalPlatform/Controllers/StudentsController.cs
+++ EducationalPlatform/EducationalPlatform/Controllers/StudentsController.cs
@@ -1,8 +1,71 @@
-﻿using System.Web.Mvc;
+﻿using EducationalPlatform.Models;
+using EducationalPlatform.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EducationalPlatform.Services;
 
 namespace EducationalPlatform.Controllers
 {
     public class StudentsController : Controller
     {
+        ApplicationDbContext _context;
+
+        public StudentsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+
+        public ActionResult Index(string id)
+        {
+            var students = _context.Students.Include(s => s.ApplicationUser).Include(s => s.Specialization).Include(s => s.Semester).Include(s => s.Year).ToList();
+            Student theOne = new Student();
+            foreach (var student in students)
+            {
+                if (student.ApplicationUser.Id.Equals(id))
+                {
+                    theOne = student;
+                }
+            }
+            var courses = _context.Courses.Include(c => c.Teacher.ApplicationUser).Include(c => c.Specialization).Include(c => c.Semester).Include(c => c.Year).ToList();
+
+            var studentAndCourses = new StudentCourseViewModel
+            {
+                Courses = courses,
+                Student = theOne
+            };
+
+            return View(studentAndCourses);
+
+        }
+
+
+
+        public ActionResult StudentProfile(string id)
+        {
+            var student = _context.Students.Include(s => s.ApplicationUser).Include(s => s.Specialization).Include(s => s.Semester).Include(s => s.Year).SingleOrDefault(c => c.ApplicationUserId == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+
+            var courses = _context.Students.Where(s => s.StudentId == student.StudentId).SelectMany(c => c.Courses).Include(c => c.Teacher.ApplicationUser).Include(s => s.Specialization).Include(s => s.Semester).Include(s => s.Year).ToList();
+            var projects = _context.Projects.ToList();
+
+            var studentService = new StudentService();
+            var model = studentService.StudentProfile(student, courses, projects);
+
+            return View("StudentProfile", model);
+        }
     }
 }
---
+++ EducationalPlatform/EducationalPlatform/Services/StudentService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using EducationalPlatform.Models;
+using EducationalPlatform.ViewModels;
+
+namespace EducationalPlatform.Services
+{
+    public class StudentService
+    {
+        public StudentCoursesProjectsViewModel StudentProfile(Student student, List<Course> courses, List<Project> projects)
+        {
+            foreach (var course in courses)
+            {
+                var project = projects.SingleOrDefault(p => p.CourseId == course.CourseId);
+                if (project != null)
+                {
+                    projects.Add(project);
+                }
+
+            }
+
+            var studentCoursesProjects = new StudentCoursesProjectsViewModel
+            {
+                Student = student,
+                Courses = courses,
+                Projects = projects
+            };
+
+            return studentCoursesProjects;
+        }
+    }
+}
