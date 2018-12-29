4601be0f15757fe16214e0d6d3a3acbe6ccdb4e4
enzojack <lc.mihut@gmail.com>
2018-12-29 19:56:53 +0200

    Included Courses and Projects in application

---
+++ EducationalPlatform/EducationalPlatform/Controllers/CoursesController.cs
@@ -0,0 +1,188 @@
+﻿using EducationalPlatform.Models;
+using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using EducationalPlatform.ViewModels;
+using System.Web.Http;
+
+namespace EducationalPlatform.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CoursesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Courses
+        public ActionResult Index(int id, string studentId)
+        {
+            var student = _context.Students.Include(s => s.ApplicationUser).SingleOrDefault(s => s.ApplicationUserId == studentId);
+            var course = _context.Courses.Include(c => c.Teacher.ApplicationUser).Include(c => c.Students).Include(c => c.Specialization).Include(c => c.Semester).Include(c => c.Year).SingleOrDefault(c => c.CourseId == id);
+            var files = _context.Files.Where(f => f.CourseId == id).ToList();
+            var projects = _context.Projects.Where(p => p.CourseId == id).ToList();
+
+            if (course.Students.Contains(student))
+            {
+                var courseFilesProjects = new CourseFilesProjects
+                {
+                    Student = student,
+                    Course = course,
+                    Files = files,
+                    Projects = projects
+                };
+                return View(courseFilesProjects);
+            }
+            else
+            {
+                var courseFilesProjects = new CourseFilesProjects
+                {
+                    Student = student,
+                    Course = course,
+                    Files = files
+                };
+                return View(courseFilesProjects);
+            }
+
+        }
+
+        [ValidateInput(false)]
+        [System.Web.Mvc.HttpPost]
+        public ActionResult RegisterToCourse(string id, [FromUri] int courseId)
+        {
+            var course = _context.Courses.SingleOrDefault(c => c.CourseId == courseId);
+            var student = _context.Students.Include(s => s.Courses).SingleOrDefault(s => s.ApplicationUserId == id);
+
+            student.Courses.Add(course);
+
+            _context.SaveChanges();
+
+            var values = new RouteValueDictionary();
+            values.Add("id", courseId);
+            values.Add("studentId", id);
+
+            return RedirectToAction("Index", values);
+
+        }
+
+        //public ActionResult Details(int id)
+        //{
+        //    var course = _context.Courses.SingleOrDefault(c => c.CourseId == id);
+
+        //    if(course == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+
+        //    return View(course);
+        //}
+        public ActionResult Details(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [System.Web.Http.HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(int id, CreateCourseViewModel newCourse)
+        {
+
+            var course = newCourse.Course;
+            course.TeacherId = id;
+            if (course.CourseId == 0)
+                _context.Courses.Add(course);
+            else
+            {
+                var courseInDb = _context.Courses.Single(c => c.CourseId == course.CourseId);
+                courseInDb.CourseName = course.CourseName;
+                courseInDb.CourseDescription = course.CourseDescription;
+                courseInDb.SpecializationId = course.SpecializationId;
+                courseInDb.SemesterId = course.SemesterId;
+                courseInDb.YearId = course.YearId;
+                courseInDb.TeacherId = course.TeacherId;
+            }
+
+
+            _context.SaveChanges();
+
+            var teacher = _context.Teachers.SingleOrDefault(t => t.TeacherId == id);
+
+            return RedirectToAction("Index/" + teacher.ApplicationUserId, "Teacher");
+        }
+
+        public ActionResult EditCourse(int id)
+        {
+            var course = _context.Courses.SingleOrDefault(c => c.CourseId == id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            var specialization = _context.Specializations.ToList();
+            var semester = _context.Semesters.ToList();
+            var year = _context.Years.ToList();
+            var project = _context.Projects.Where(p => p.CourseId == id).ToList();
+
+            if (specialization == null || semester == null || year == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            var createNewCourse = new CreateCourseViewModel
+            {
+                TeacherId = course.TeacherId.Value,
+                Specializations = specialization,
+                Semesters = semester,
+                Years = year,
+                Course = course,
+                Projects = project
+
+            };
+
+
+            return View("CreateOrUpdateCourse", createNewCourse);
+
+        }
+
+
+
+        public ActionResult CreateOrUpdateCourse(int? teacherId)
+        {
+
+            //var teachers = _context.Teachers.Include(c => c.ApplicationUser).ToList();
+            var specialization = _context.Specializations.ToList();
+            var semester = _context.Semesters.ToList();
+            var year = _context.Years.ToList();
+
+            if (specialization == null || semester == null || year == null)
+            {
+                return HttpNotFound();
+            }
+
+            var createNewCourse = new CreateCourseViewModel
+            {
+                TeacherId = teacherId.Value,
+                Specializations = specialization,
+                Semesters = semester,
+                Years = year
+            };
+
+
+            return View("CreateOrUpdateCourse", createNewCourse);
+        }
+
+
+    }
+}
---
+++ EducationalPlatform/EducationalPlatform/Controllers/ProjectsController.cs
@@ -0,0 +1,219 @@
+﻿using EducationalPlatform.Models;
+using EducationalPlatform.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Data.Entity;
+
+namespace EducationalPlatform.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProjectsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Projects
+        public ActionResult CreateNewProject(int courseId)
+        {
+            var newProject = new Project
+            {
+                CourseId = courseId,
+            };
+
+
+            return View("CreateNewProject", newProject);
+
+
+        }
+
+        public ActionResult AddFiles(int id)
+        {
+            var project = _context.Projects.SingleOrDefault(c => c.Id == id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("CreateNewProject", project);
+
+        }
+
+
+        [System.Web.Http.HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save([FromUri]int courseId, Project newProject)
+        {
+
+            newProject.CourseId = courseId;
+            if (newProject.Id == 0)
+                _context.Projects.Add(newProject);
+            else
+            {
+                var projectInDb = _context.Projects.Single(c => c.Id == newProject.Id);
+                projectInDb.ProjectName = newProject.ProjectName;
+                projectInDb.ProjectDeadline = newProject.ProjectDeadline;
+                projectInDb.CourseId = newProject.CourseId;
+
+            }
+
+            _context.SaveChanges();
+
+
+
+            return RedirectToAction("EditCourse/" + courseId, "Courses");
+        }
+
+        public ActionResult UploadFileForProject(int id)
+        {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+            return View("UploadFilesForProject", project);
+        }
+
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult SaveFileToDatabase(int id, HttpPostedFileBase file)
+        {
+            int fileSize = file.ContentLength;
+            String FileExt = Path.GetExtension(file.FileName).ToUpper();
+            if (FileExt == ".PDF" || FileExt == ".RAR")
+            {
+                //get the bytes from the uploaded file
+                byte[] data = GetBytesFromFile(file);
+
+                var fileToSave = new Models.File
+                {
+                    ProjectId = id,
+                    FileName = file.FileName,
+                    FileContent = data,
+                    UploadedDate = DateTime.Now,
+                    Size = fileSize
+
+
+                };
+                _context.Files.Add(fileToSave);
+                _context.SaveChanges();
+
+
+            }
+
+
+            return RedirectToAction("UploadFileForProject/" + id, "Projects");
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult SaveFileToDatabaseForStudent(int id, int projectId, HttpPostedFileBase file)
+        {
+            int fileSize = file.ContentLength;
+            String FileExt = Path.GetExtension(file.FileName).ToUpper();
+            if (FileExt == ".RAR")
+            {
+                //get the bytes from the uploaded file
+                byte[] data = GetBytesFromFile(file);
+
+
+                var projectFileDB = _context.Files.FirstOrDefault(f => f.ProjectId == projectId && f.StudentId == id);
+                if (projectFileDB == null)
+                {
+                    var fileToSave = new Models.File
+                    {
+                        ProjectId = projectId,
+                        StudentId = id,
+                        FileName = file.FileName,
+                        FileContent = data,
+                        UploadedDate = DateTime.Now,
+                        Size = fileSize
+
+
+                    };
+                    _context.Files.Add(fileToSave);
+                }
+                else
+                {
+                    projectFileDB.FileName = file.FileName;
+                    projectFileDB.FileContent = data;
+                    projectFileDB.UploadedDate = DateTime.Now;
+                    projectFileDB.Size = fileSize;
+                }
+
+                _context.SaveChanges();
+
+
+            }
+            var student = _context.Students.SingleOrDefault(s => s.StudentId == id);
+            var values = new RouteValueDictionary();
+            values.Add("projectId", projectId);
+            values.Add("id", student.ApplicationUserId);
+            return RedirectToAction("StudentProject", values);
+        }
+
+        private byte[] GetBytesFromFile(HttpPostedFileBase file)
+        {
+            using (Stream inputStream = file.InputStream)
+            {
+                MemoryStream memoryStream = inputStream as MemoryStream;
+                if (memoryStream == null)
+                {
+                    memoryStream = new MemoryStream();
+                    inputStream.CopyTo(memoryStream);
+                }
+                return memoryStream.ToArray();
+            }
+        }
+
+
+        public ActionResult StudentProject(string id, int projectId)
+        {
+            var student = _context.Students.Include(s => s.Projects).SingleOrDefault(s => s.ApplicationUserId == id);
+            var project = _context.Projects.SingleOrDefault(p => p.Id == projectId);
+            var filesForProject = _context.Files.Where(f => f.ProjectId == projectId);
+            var fileUploadedByStudent = _context.Files.SingleOrDefault(f => f.StudentId == student.StudentId && f.ProjectId == projectId);
+            if (!student.Projects.Contains(project))
+            {
+                student.Projects.Add(project);
+                _context.SaveChanges();
+            }
+
+            var studentProjectFiles = new StudentProjectFilesViewModel
+            {
+                Student = student,
+                Project = project,
+                ProjectAdditionalFiles = filesForProject,
+                StudentUploadedFile = fileUploadedByStudent
+            };
+
+
+            return View(studentProjectFiles);
+        }
+
+        public ActionResult ViewUploadedProjects(int projectId)
+        {
+            var project = _context.Projects.Include(p => p.Students).SingleOrDefault(p => p.Id == projectId);
+            return View(project);
+        }
+
+
+
+        public FileResult DownLoadFile(int id)
+        {
+            var fileSelected = _context.Files.SingleOrDefault(file => file.Id == id);
+
+            return File(fileSelected.FileContent, "App_Data/pdf", fileSelected.FileName);
+        }
+
+    }
+}
